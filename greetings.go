86246043e5040869ab69911b45b50f210043648f
kenneth ros <zhaho.ros@gmail.com>
2025-01-29 18:25:20 +0100

    feat: added randomized greetings

--- greetings.go
+++ greetings.go
@@ -3,6 +3,7 @@ package greetings
 import (
 	"fmt"
 	"errors"
+	"math/rand"
 )
 
 // Hello returns a greeting for the named person
@@ -11,7 +12,17 @@ func Hello(name string) (string, error) {
 	if name == "" {
 			return "", errors.New("empty name")
 	}
-	// Return a greeting that embeds the name in a message
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v",
+		"Hail allmighty %v!, well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
