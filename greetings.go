543b71e7bbe7a09bb6107507bb7f3159df7be19f
kenneth ros <zhaho.ros@gmail.com>
2025-01-29 18:40:27 +0100

    fix: minor update

--- greetings.go
+++ greetings.go
@@ -1,28 +1,33 @@
 package greetings
 
 import (
-	"fmt"
-	"errors"
-	"math/rand"
+    "errors"
+    "fmt"
+    "math/rand"
 )
 
-// Hello returns a greeting for the named person
+// Hello returns a greeting for the named person.
 func Hello(name string) (string, error) {
-	// If no name was given, return an error
-	if name == "" {
-			return "", errors.New("empty name")
-	}
-	
-	message := fmt.Sprintf(randomFormat(), name)
-	return message, nil
+    // If no name was given, return an error with a message.
+    if name == "" {
+        return name, errors.New("empty name")
+    }
+    // Create a message using a random format.
+    message := fmt.Sprintf(randomFormat(), name)
+    return message, nil
 }
 
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
 func randomFormat() string {
-	formats := []string{
-		"Hi, %v. Welcome!",
-		"Great to see you, %v",
-		"Hail allmighty %v!, well met!",
-	}
+    // A slice of message formats.
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
 
-	return formats[rand.Intn(len(formats))]
+    // Return a randomly selected message format by specifying
+    // a random index for the slice of formats.
+    return formats[rand.Intn(len(formats))]
 }
